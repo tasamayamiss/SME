3d2eec23db88a62d87049140484cfddabc9333c1
tasamayamiss <77745984+tasamayamiss@users.noreply.github.com>
2025-03-26 18:26:25 +0300

    Add files via upload

--- contacts.php
+++ contacts.php
@@ -1,17 +1,12 @@
 <?php
 $to = "ya-zhanna.balash@yandex.ru"; // емайл получателя данных из формы
 $tema = "Форма обратной связи на РНР"; // тема полученного емайла
-$message = "Ваше имя: " $_POST [' name ']."<br>";//
-присвоить переменной значение, полученное из
-формы name=name
-$message .= "E-mail: ""$_POST['email']."<br>";
-/ /полученное из формы name=email $message .= "Номер телефона: ""$_POST['number']."
-<br>"; // полученное из формы name=phone
-$message = "Сообщение: " $_POST [ 'message']."
-<br>"; // полученное из формы name=message
+$message = "Ваше имя: " .$_POST [' name ']."<br>";
+$message .= "E-mail: ".$_POST['email']."<br>";
+$message .= "Номер телефона: ".$_POST['number']."<br>"; 
+$message .= "Сообщение: " .$_POST [ 'message']."<br>"; 
 $headers = 'MIME-Version: 1.0' . "\r\n"; 
 $headers .= 'Content-type: text/html;
 charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //
-отправляет получателю на емайл значения переменных
+mail($to, $tema, $message, $headers); 
 ?>
